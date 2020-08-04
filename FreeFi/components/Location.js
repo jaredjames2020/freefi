f70956cc6aa58898fa0639fa4c828e769b11596e
jaredjames2020 <jaredjames2020@gmail.com>
2020-08-04 19:06:15 -0400

    git

---
+++ FreeFi/components/Location.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View } from "react-native";
+
+function Location(props) {
+  return (
+    <View>
+      <Text>JKJLJ</Text>
+    </View>
+  );
+}
+
+export default Location;
