5af61d544f7a0873570317ab4e58e800d288324b
jaredjames2020 <jaredjames2020@gmail.com>
2020-08-04 21:20:39 -0400

    add user location component

--- FreeFi/screens/WelcomeScreen.js
+++ FreeFi/screens/WelcomeScreen.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { View, StyleSheet, Text } from "react-native";
 
+import useLocation from "../hooks/useLocation";
+
 function WelcomeScreen(props) {
-  return (
-    <View style={styles.container}>
-      <Text>WELCOME SCREEN</Text>
-    </View>
-  );
+  const location = useLocation();
+  console.log(location);
+  return <View style={styles.container}></View>;
 }
 
 const styles = StyleSheet.create({
