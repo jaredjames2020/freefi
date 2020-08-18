28b5b6bb9ac06085bb99d5365e7ba2c598b918a2
jaredjames2020 <jaredjames2020@gmail.com>
2020-08-18 17:33:38 -0400

    refactor to separate concerns

--- FreeFi/app/screens/MapScreen.js
+++ FreeFi/app/screens/MapScreen.js
@@ -1,24 +1,49 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 
-import MapView from "react-native-maps";
+import getNearbyLocations from "../hooks/getNearbyLocations";
+import getData from "../hooks/getData";
+import getMarker from "../hooks/getMarker";
+
+import MapView, { Marker } from "react-native-maps";
+import useLocation from "../hooks/useLocation";
 
 function MapScreen(props) {
-  console.log("YUP");
+  const screen = Dimensions.get("window");
+  const data = getData();
+  const nearbyLocations = getNearbyLocations(data);
+  const marker = getMarker();
+  const location = useLocation();
+
+  const ASPECT_RATIO = screen.width / screen.height;
+  const LATITUDE_DELTA = 0.0922;
+  const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
   return (
     <MapView
-      initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
+      style={styles.map}
+      region={{
+        latitude: location.latitude,
+        longitude: location.longitude,
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
       }}
-    />
+      showsUserLocation={true}
+    >
+      {marker}
+    </MapView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
 });
 
 export default MapScreen;
