62860686cc59196de9bc5c789a75eeff7301b7c1
jaredjames2020 <jaredjames2020@gmail.com>
2020-08-23 19:09:51 -0400

    refactor search input

--- FreeFi/app/components/BottomSearchInput.js
+++ FreeFi/app/components/BottomSearchInput.js
@@ -1,23 +1,32 @@
-import React, { Component } from "react";
-import { TextInput } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, TextInput, Button } from "react-native";
+import useTextInput from "../hooks/useTextInput";
+import getLocationFromString from "../hooks/getLocationFromString";
 
 const BottomSearchInput = () => {
-  const [value, onChangeText] = React.useState("SEARCH");
+  const [text, setText] = useState();
 
   return (
-    <TextInput
-      style={{
-        margin: 15,
-        height: 40,
-        width: "95%",
-        borderColor: "black",
-        borderWidth: 3,
-        padding: 7,
-        borderRadius: 10,
-      }}
-      onChangeText={(text) => onChangeText(text)}
-      value={value}
-    />
+    <View>
+      <TextInput
+        style={{
+          margin: 15,
+          height: 40,
+          width: "95%",
+          borderColor: "black",
+          borderWidth: 3,
+          padding: 7,
+          borderRadius: 10,
+        }}
+        onChangeText={(text) => setText(text)}
+        value={text}
+        placeholder="Search location"
+      />
+      <Button
+        onPress={() => getLocationFromString(text)}
+        title="Click"
+      ></Button>
+    </View>
   );
 };
 
